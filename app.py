758e0735fa192cba546012b9af57f04a4b09a1cc
anabanadvali <bekabitarashvili@gmail.com>
2024-07-11 16:39:34 +0400

    Structure

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 class PDF(FPDF):
     def header(self):
         self.set_font('DejaVu', 'B', 12)
-        self.cell(0, 10, 'Bug Report', 0, 1, 'C')
+        self.cell(100, 10, 'რეპორტი ხარვეზებისათვის', 0, 1, 'C')
 
     def footer(self):
         self.set_y(-15)
@@ -29,10 +29,10 @@ def submit():
     pdf.add_font('DejaVu', 'B', 'static/fonts/DejaVuSans-Bold.ttf', uni=True)
     pdf.add_font('DejaVu', 'I', 'static/fonts/DejaVuSans-Oblique.ttf', uni=True)
     pdf.add_page()
-    pdf.set_font('DejaVu', '', 14)
+    pdf.set_font('DejaVu', '', 8)
 
     for key, value in data.items():
-        pdf.cell(0, 10, txt=f'{key.capitalize()}: {value}', ln=True)
+        pdf.cell(100, 5, txt=f'{key.capitalize()}: {value}', ln=True)
 
     pdf_output = 'bug_report.pdf'
     pdf.output(pdf_output)
